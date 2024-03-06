b0ac5d2a4b6b7e421d53234e61f786359e887356
Martijn Dirksen <martijn.dirksen@protonmail.com>
2024-03-06 20:48:35 +0100

    wip

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 - [ ] Fix bug where array index-based access gets typed as any
 - [ ] Create tests for for-loop
 - [ ] Array index writing does not use gradual typing, what is the index-based access type?
+- [ ] Write something about not having support for multidimensional arrays
--- samples/6-array/array.ts
+++ samples/6-array/array.ts
@@ -1,10 +1,9 @@
 var arr: number[] = [];
 var arr2: number[] = [2, 4, 7];
-var arr3: number[][] = [[2], [4], arr2];
-var arr4: string[] = ['str1', 'str2', 'str3'];
+var arr3: string[] = ['str1', 'str2', 'str3'];
 
-var input: any = 'str4';
-arr4.push(input);
-var str = arr4[0] + arr4[1];
+var input: any = 3;
+arr3.push(input);
+var str = arr3[0] + arr3[1];
 
-arr4[1] = input;
+arr3[1] = input;
